e279b2cf7f2d1c50016b5e3869fca791a737ac11
jorge <jorgemorenodev@gmail.com>
2025-01-27 18:09:59 +0100

    add own code for labs 1,2,3 for Week 2

--- bootcamp/materials/2-fact-data-modeling/lab_scripts/day2.sql
+++ bootcamp/materials/2-fact-data-modeling/lab_scripts/day2.sql
@@ -1,88 +1,87 @@
---
+select * from events;
 
--- There's duplicated data so we need to remove it
-
-SELECT game_id, team_id, player_id, count(*)
-FROM game_details
-group by game_id, team_id, player_id 
-having count(*) > 1;
-
-
--- Create DDL
-CREATE TABLE fct_game_details (
-    dim_game_date date,
-    dim_season int,
-    dim_team_id int,
-    dim_player_id int,
-    dim_player_name text,
-    dim_start_position text,
-    dim_is_playing_at_home boolean,
-    dim_did_not_play boolean,
-    dim_did_not_dress boolean,
-    dim_not_with_team boolean,
-    m_minutes REAL,
-    m_fgm int,
-    m_fga int,
-    m_fg3m int,
-    m_fg3a int,
-    m_oreb int,
-    m_dreb int,
-    m_reb int,
-    m_ast int,
-    m_stl int,
-    m_blk int,
-    m_turnovers int,
-    m_pf int,
-    m_pts int,
-    m_plus_minus int,
-    PRIMARY KEY (dim_game_date, dim_team_id, dim_player_id)
+CREATE TABLE users_cumulated (
+    user_id TEXT,
+    -- List of dates in the past where the user was active
+    dates_active DATE[],
+    date DATE,
+    PRIMARY KEY (user_id, date)
 );
 
-
-INSERT INTO fct_game_details
-WITH deduped AS (
-    SELECT
-        gd.*,
-        g.game_date_est,
-        g.season,
-        g.home_team_id,
-        ROW_NUMBER() OVER (PARTITION BY gd.game_id, team_id, player_id ORDER BY g.game_date_est) AS row_num
-    FROM game_details gd
-             -- Since we don't have a when, we need to join with the games table to get the date
-             JOIN games g on gd.game_id = g.game_id
+INSERT INTO users_cumulated
+WITH yesterday AS (
+    SELECT 
+        *
+    FROM users_cumulated
+    WHERE date = DATE('2023-01-27')
+),
+    today AS (
+    SELECT 
+        user_id::TEXT,
+        DATE(CAST(event_time AS TIMESTAMP)) as date_active
+    FROM events
+    WHERE DATE(CAST(event_time AS TIMESTAMP)) = DATE('2023-01-28')
+        AND user_id IS NOT NULL
+    GROUP BY user_id, DATE(CAST(event_time AS TIMESTAMP))
 )
-SELECT
-    game_date_est as dim_game_date,
-    season as dim_season,
-    team_id as dim_team_id,
-    player_id as dim_player_id,
-    player_name as dim_player_name,
-    start_position as dim_start_position,
-    team_id = home_team_id AS dim_is_playing_at_home,
-    COALESCE(POSITION('DNP' IN comment), 0) > 0 AS dim_did_not_play,
-    COALESCE(POSITION('DND' IN comment), 0) > 0 AS dim_did_not_dress,
-    COALESCE(POSITION('NWT' IN comment), 0) > 0 AS dim_not_with_team,
-    CAST(SPLIT_PART(min, ':', 1)::INT AS REAL) + CAST(SPLIT_PART(min, ':', 2)::INT AS REAL)/60 m_minutes,
-    fgm AS m_fgm,
-    fga AS m_fga,
-    fg3m AS m_fg3m,
-    fg3a AS m_fg3a,
-    oreb AS m_oreb,
-    dreb AS m_dreb,
-    reb AS m_reb,
-    ast AS m_ast,
-    stl AS m_stl,
-    blk AS m_blk,
-    "TO" as m_turnovers,
-    pf as m_pf,
-    pts as m_pts,
-    plus_minus as m_plus_minus
-FROM deduped WHERE row_num = 1;
+SELECT 
+    COALESCE(t.user_id, y.user_id) as user_id,
+    CASE WHEN y.dates_active IS NULL THEN ARRAY[t.date_active]
+        WHEN t.date_active IS NULL THEN y.dates_active
+        ELSE t.date_active || y.dates_active
+    END as dates_active,
+    COALESCE(t.date_active, y.date + INTERVAL '1 DAY') as date
+FROM today t
+FULL OUTER JOIN yesterday y 
+    ON t.user_id = y.user_id;
 
--- Usage example - find the bailed rate of each player
-SELECT dim_player_name,
-       COUNT(*) as num_games,
-       COUNT(CASE WHEN dim_not_with_team THEN 1 END) as bailed_num,
-       CAST(COUNT(CASE WHEN dim_not_with_team THEN 1 END) AS REAL) / COUNT(*) as bailed_rate
-FROM fct_game_details
-GROUP BY dim_player_name;
+
+SELECT *
+FROM users_cumulated
+WHERE date = DATE('2023-01-28')
+LIMIT 100;
+
+
+-- You can use now this cumulated table to extract information like
+    -- was the user active yesterday?
+    -- was the user active in the last month? and week?
+    -- how many days in a row has the user been active?
+    -- how many days in the last month has the user been active?
+-- (Each month is considered of 28 days here so there's the same amount of dow)
+-- We use bitwise operators to check if the user was active on a given date
+
+WITH users AS (
+    SELECT * 
+    FROM users_cumulated
+    WHERE date = DATE('2023-01-28')
+),
+series AS (
+    SELECT * FROM generate_series(DATE('2023-01-01'), DATE('2023-01-28'), INTERVAL '1 DAY') as series_date
+),
+placeholder_ints AS (
+    SELECT 
+        CASE WHEN dates_active @> ARRAY [DATE(series_date)]
+            THEN CAST(POW(2, 32-(date-DATE(series_date))) AS BIGINT) 
+            ELSE 0
+        END as placeholder_int_value,
+        *
+    FROM users CROSS JOIN series
+),
+user_activity AS (
+    SELECT
+        user_id,
+        CAST(CAST(SUM(placeholder_int_value) AS BIGINT) AS BIT(32)) as active_days
+    FROM placeholder_ints
+    GROUP BY user_id 
+)
+SELECT 
+    user_id,
+    active_days,
+    BIT_COUNT(active_days) as dim_num_active_days_last_month,
+    BIT_COUNT(active_days) > 0 as dim_is_active_last_month,
+    BIT_COUNT(active_days & B'11111110000000000000000000000000') as dim_num_active_days_last_week,
+    BIT_COUNT(active_days & B'11111110000000000000000000000000') > 0 as dim_is_active_last_week,
+    BIT_COUNT(active_days & B'10000000000000000000000000000000') > 0 as dim_is_active_last_day
+FROM user_activity
+WHERE user_id = '137925124111668560'
+
--- bootcamp/materials/2-fact-data-modeling/lab_scripts/day3.sql
+++ bootcamp/materials/2-fact-data-modeling/lab_scripts/day3.sql
@@ -1,87 +1,62 @@
-select * from events;
-
-CREATE TABLE users_cumulated (
-    user_id TEXT,
-    -- List of dates in the past where the user was active
-    dates_active DATE[],
-    date DATE,
-    PRIMARY KEY (user_id, date)
+CREATE TABLE array_metrics (
+    user_id NUMERIC,
+    month_start DATE,
+    metric_name TEXT,
+    metric_array REAL[],
+    PRIMARY KEY (user_id, month_start, metric_name)
 );
 
-INSERT INTO users_cumulated
-WITH yesterday AS (
-    SELECT 
-        *
-    FROM users_cumulated
-    WHERE date = DATE('2023-01-27')
-),
-    today AS (
-    SELECT 
-        user_id::TEXT,
-        DATE(CAST(event_time AS TIMESTAMP)) as date_active
-    FROM events
-    WHERE DATE(CAST(event_time AS TIMESTAMP)) = DATE('2023-01-28')
-        AND user_id IS NOT NULL
-    GROUP BY user_id, DATE(CAST(event_time AS TIMESTAMP))
-)
-SELECT 
-    COALESCE(t.user_id, y.user_id) as user_id,
-    CASE WHEN y.dates_active IS NULL THEN ARRAY[t.date_active]
-        WHEN t.date_active IS NULL THEN y.dates_active
-        ELSE t.date_active || y.dates_active
-    END as dates_active,
-    COALESCE(t.date_active, y.date + INTERVAL '1 DAY') as date
-FROM today t
-FULL OUTER JOIN yesterday y 
-    ON t.user_id = y.user_id;
 
-
-SELECT *
-FROM users_cumulated
-WHERE date = DATE('2023-01-28')
-LIMIT 100;
-
-
--- You can use now this cumulated table to extract information like
-    -- was the user active yesterday?
-    -- was the user active in the last month? and week?
-    -- how many days in a row has the user been active?
-    -- how many days in the last month has the user been active?
--- (Each month is considered of 28 days here so there's the same amount of dow)
--- We use bitwise operators to check if the user was active on a given date
-
-WITH users AS (
-    SELECT * 
-    FROM users_cumulated
-    WHERE date = DATE('2023-01-28')
-),
-series AS (
-    SELECT * FROM generate_series(DATE('2023-01-01'), DATE('2023-01-28'), INTERVAL '1 DAY') as series_date
-),
-placeholder_ints AS (
-    SELECT 
-        CASE WHEN dates_active @> ARRAY [DATE(series_date)]
-            THEN CAST(POW(2, 32-(date-DATE(series_date))) AS BIGINT) 
-            ELSE 0
-        END as placeholder_int_value,
-        *
-    FROM users CROSS JOIN series
-),
-user_activity AS (
+INSERT INTO array_metrics
+WITH daily_aggregate AS (
     SELECT
         user_id,
-        CAST(CAST(SUM(placeholder_int_value) AS BIGINT) AS BIT(32)) as active_days
-    FROM placeholder_ints
-    GROUP BY user_id 
+        DATE(event_time) AS date,
+        COUNT(*) AS num_site_hits
+    FROM events
+    WHERE DATE(event_time) = DATE('2023-01-03')
+    AND user_id IS NOT NULL
+    GROUP BY user_id, DATE(event_time)
+), yesterday_array AS (
+    SELECT *
+    FROM array_metrics
+    WHERE month_start = DATE('2023-01-01')
 )
 SELECT 
-    user_id,
-    active_days,
-    BIT_COUNT(active_days) as dim_num_active_days_last_month,
-    BIT_COUNT(active_days) > 0 as dim_is_active_last_month,
-    BIT_COUNT(active_days & B'11111110000000000000000000000000') as dim_num_active_days_last_week,
-    BIT_COUNT(active_days & B'11111110000000000000000000000000') > 0 as dim_is_active_last_week,
-    BIT_COUNT(active_days & B'10000000000000000000000000000000') > 0 as dim_is_active_last_day
-FROM user_activity
-WHERE user_id = '137925124111668560'
+    COALESCE(da.user_id, ya.user_id) AS user_id,
+    COALESCE(ya.month_start, DATE_TRUNC('month', da.date)) AS month_start,
+    'site_hits' AS metric_name,
+    CASE WHEN ya.metric_array IS NOT NULL
+        THEN ya.metric_array || ARRAY[COALESCE(da.num_site_hits, 0)]
+        ELSE ARRAY_FILL(0, ARRAY[date-DATE(DATE_TRUNC('month', da.date))]) || ARRAY[COALESCE(da.num_site_hits, 0)]
+    END AS metric_array
+FROM daily_aggregate da
+    FULL OUTER JOIN yesterday_array ya ON da.user_id = ya.user_id
+ON CONFLICT (user_id, month_start, metric_name) DO UPDATE
+    SET metric_array = EXCLUDED.metric_array;
+;
+
+SELECT *
+FROM array_metrics;
+
+
+-- Example of how to go back to daily aggregates with this
+
+WITH agg AS (
+    SELECT 
+        metric_name,
+        month_start,
+        ARRAY[
+            SUM(metric_array[1]),
+            SUM(metric_array[2]),
+            SUM(metric_array[3])
+        ] AS summed_array
+    FROM array_metrics
+    GROUP BY metric_name, month_start
+)
+SELECT 
+    metric_name,
+    month_start + CAST(index AS INT) - 1 AS date,
+    elem as value
+FROM agg CROSS JOIN UNNEST(agg.summed_array) WITH ORDINALITY AS a(elem, index)
 
